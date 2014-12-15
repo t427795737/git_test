d73eae63f293d4647218984e6f0c024b942aa3e9
t427795737 <tianof427795737@126.com>
2014-12-15 16:15:42 +0800

    [TestGit2][Test22]

---
+++ src/Test22.java
@@ -0,0 +1,39 @@
+/**  
+ * @Title:  Test22.java
+ * @Package 
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author wangxiaoming
+ * @date  Dec 15, 2014 4:14:25 PM
+ * @version V1.0  
+ * Update Logs:
+ * ****************************************************
+ * Name:
+ * Date:
+ * Description:
+ ******************************************************
+ */
+
+/**
+ * @ClassName: Test22
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author wangxiaoming
+ * @date Dec 15, 2014 4:14:25 PM
+ *
+ */
+public class Test22
+{
+
+    /**
+     * @Title: main
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author wangxiaoming
+     * @param args
+     * @throws
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("TestGit2 test2 fuck your sister");
+    }
+
+}
